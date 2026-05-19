b5058f656d20e8a5c76a00b8c008620ac5221efd
Pak Stanislav <pakstas801@gmail.com>
2026-05-19 18:28:59 +0600

    Search done on V

--- src/Search.java
+++ src/Search.java
@@ -1,31 +1,28 @@
 import java.util.*;
 
-public class Search<Vertex> {
-    protected Set<Vertex> marked;
-    protected Map<Vertex, Vertex> edgeTo;
-    protected final Vertex source;
+public abstract class Search<V> {
+    protected Set<Vertex<V>> marked;
+    protected Map<Vertex<V>, Vertex<V>> edgeTo;
+    protected final Vertex<V> source;
 
-    public Search(Vertex source) {
+    public Search(Vertex<V> source) {
         this.source = source;
         marked = new HashSet<>();
         edgeTo = new HashMap<>();
     }
 
-    public boolean hasPathTo(Vertex v) {
+    public boolean hasPathTo(Vertex<V> v) {
         return marked.contains(v);
     }
 
-    public Iterable<Vertex> pathTo(Vertex v) {
+    public Iterable<Vertex<V>> pathTo(Vertex<V> v) {
         if (!hasPathTo(v)) return null;
-
-        LinkedList<Vertex> ls = new LinkedList<>();
-        for (Vertex i = v; i != source; i = edgeTo.get(i)) {
-            ls.push(i); // inverted adding
+        LinkedList<Vertex<V>> path = new LinkedList<>();
+        for (Vertex<V> i = v; i != source; i = edgeTo.get(i)) {
+            path.push(i);
         }
-
-        ls.push(source);
-
-        return ls;
+        path.push(source);
+        return path;
     }
 }
 
